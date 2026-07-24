3666ceddf3511285c4d070a6735cf8f9214d3153
harshi-295v <gudeharshitha105@gmail.com>
2026-07-24 21:28:08 +0530

    Remove duplicate nested folder

--- DeepSkilling/Module 6 - (ASP.NET Core Web API)/FirstWebApi/FirstWebApi/Models/Department.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FirstWebApi.Models
-{
-    public class Department
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; } = string.Empty;
-    }
-}
--- DeepSkilling/Module 6 - (ASP.NET Core Web API)/FirstWebApi/FirstWebApi/Models/Skill.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace FirstWebApi.Models
-{
-    public class Skill
-    {
-        public int Id { get; set; }
-
-        public string Name { get; set; } = string.Empty;
-    }
-}
